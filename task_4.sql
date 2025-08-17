ecbfbd34ffd09e197d85100e86ee697af2201525
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-08-17 22:24:11 +0200

    Updated table description script

--- task_4.sql
+++ task_4.sql
@@ -14,7 +14,7 @@ mycursor = mydb.cursor()
 mycursor.execute("""
     SELECT COLUMN_NAME, COLUMN_TYPE
     FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books'
+    WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
 """)
 for column in mycursor.fetchall():
     print(column)
