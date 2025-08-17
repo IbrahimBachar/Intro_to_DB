a0c2bedccc94b62219dc3fae4e5da24d925e55c6
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-08-17 22:47:28 +0200

    Multiple insertion script

--- task_6.sql
+++ task_6.sql
@@ -11,7 +11,7 @@ mycursor = mydb.cursor()
 
 sql = """INSERT INTO customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"""
 val = """
-    (2, "Blessing Malik", "bmalick@sandtech.com", "124 Happiness  Ave."),
+    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
     (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
     (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
     """
