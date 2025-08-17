d1b5a7c39c5dcaef779d23351fadf0cabef79423
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-08-17 18:14:04 +0200

    Database creation using python

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,10 +7,12 @@ mydb = mysql.connector.connect(
 );
 
 mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store");
 
-
-print("Database 'alx_book_store' created successfully!")
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 mycursor.close()
 mydb.close()
