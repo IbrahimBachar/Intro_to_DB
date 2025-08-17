d3eda587e0b78361de941d87a3d80c03529211f9
Ibrahim Bachar <ibrahimabdraman43@gmail.com>
2025-08-17 22:46:02 +0200

    Multiple insertion script

---
+++ task_6.sql
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="IBRAHIM2525",
+    database="alx_book_store"
+);
+
+mycursor = mydb.cursor()
+
+sql = """INSERT INTO customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"""
+val = """
+    (2, "Blessing Malik", "bmalick@sandtech.com", "124 Happiness  Ave."),
+    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
+    """
+mycursor.execute(sql, val)
+mydb.commit()
+
+mycursor.close()
+mydb.close()
